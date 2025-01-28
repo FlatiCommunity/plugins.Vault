5a5b363ff7fc54085c36dd2e9921c35e725cc260
MrVaylis <mrvaylis@icloud.com>
2025-01-28 19:23:18 +0300

    f c

---
+++ src/main/java/me/naulbimix/fVault/FVault.java
@@ -0,0 +1,17 @@
+package me.naulbimix.fVault;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FVault extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
